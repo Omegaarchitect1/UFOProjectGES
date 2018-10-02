084779fcb5905352e38c3939d7ef250b0f66c799
Omegaarchitect1 <omegaarchitect1@gmail.com>
2018-10-01 19:02:44 -0500

    Added Player Movement scripts

---
+++ Assets/Scirpts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+ 
+    private void FixedUpdate()
+    {
+        float movementhorizontal = Input.GetAxis("Horizontal");
+        float movementvertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(movementhorizontal, movementvertical);
+        rb2d.AddForce(movement);
+    }
+
+    //Test
+}
